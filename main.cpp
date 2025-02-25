812411e42dad0766fe6271ac51c907d8a8a2a5af
SideFx <jan.buchholz@t-online.de>
2025-02-25 15:51:04 +0100

    Code cleanup

--- main.cpp
+++ main.cpp
@@ -1,3 +1,7 @@
+//-----------------------------------------------------------------------------------------------------------
+// Emby Explorer (Qt) (w) 2024-2025 by Jan Buchholz
+// Main
+//-----------------------------------------------------------------------------------------------------------
 #include "mainwindow.h"
 #include <QApplication>
 #include <QLocale>
@@ -6,17 +10,10 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-
-    QTranslator translator;
-    const QStringList uiLanguages = QLocale::system().uiLanguages();
-    for (const QString &locale : uiLanguages) {
-        const QString baseName = "EmbyExplorerQt_" + QLocale(locale).name();
-        if (translator.load(":/i18n/" + baseName)) {
-            a.installTranslator(&translator);
-            break;
-        }
+    if (QLocale::system().name().startsWith("en_", Qt::CaseSensitive)) {
+        QTranslator translator;
+        if (translator.load("EmbyExplorerQt_en_US.qm")) a.installTranslator(&translator);
     }
-
     MainWindow w;
     w.show();
     return a.exec();
