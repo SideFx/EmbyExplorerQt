b0eacd810544e53dbb3bcccf72bb4bdaf080c5e2
SideFx <jan.buchholz@t-online.de>
2025-03-01 15:00:24 +0100

    added styles, swapped toolbar icons

--- main.cpp
+++ main.cpp
@@ -6,10 +6,16 @@
 #include <QApplication>
 #include <QLocale>
 #include <QTranslator>
+#include <QStyleFactory>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    #if defined(Q_OS_WIN)
+        a.setStyle(QStyleFactory::create("windowsvista"));
+    #elif defined(Q_OS_MAC)
+        a.setStyle(QStyleFactory::create("fusion"));
+    #endif
     if (QLocale::system().name().startsWith("en_", Qt::CaseSensitive)) {
         QTranslator translator;
         if (translator.load("EmbyExplorerQt_en_US.qm")) a.installTranslator(&translator);
